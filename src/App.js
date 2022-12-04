e8057f8b03cb11f813f2778a35bf9d17b5b00fc2
healthybanana <iketo.healthy@gmail.com>
2022-12-05 05:09:28 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,71 +1,98 @@
 import './App.css';
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { connect } from "@argent/get-starknet"
+import erc20Abi from "./abi/erc20_abi.json"
+import whitelistPresaleAbi from "./abi/whitelistPresale_abi.json"
+import { Contract } from "starknet"
 
-const contractAddress = "0x049e5c0e9fbb072d7f908e77e117c76d026b8daf9720fe1d74fa3309645eabce"
+const ethContractAddress = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7"
+const whitelistContractAddress = "0x0787394ffbae37324f14417635267909607f051a580aee93a71b4998b6dd417c"
+const whitelistContractAddressInFelt = "3405109849710553724581303656748743505005861533151013043845065156189463855484"
 
 function App() {
-  const [provider, setProvider] = useState('')
-  const [address, setAddress] = useState('')
-  const [name, setName] = useState('')
-  const [inputAddress, setInputAddress] = useState('')
-  const [retrievedName, setRetrievedName] = useState('')
-  const [isConnected, setIsConnected] = useState(false)
+	const [provider, setProvider] = useState('')
+	const [address, setAddress] = useState('')
+	const [isConnected, setIsConnected] = useState(false)
 
-  const connectWallet = async() => {
-    
-  }
+	const connectWallet = async () => {
+		try {
+			const starknet = await connect()
+			await starknet?.enable({ starknetVersion: "v4" })
+			setProvider(starknet.account)
+			setAddress(starknet.selectedAddress)
+			setIsConnected(true)
+		}
+		catch (e) {
+			alert(e.message)
+		}
 
-  const setNameFunction = async() => {
-    
-  }
+	}
 
-  const getNameFunction = async() => {
-    
-  }
+	const approveEth = async () => {
+		try {
+			const ethContract = new Contract(erc20Abi, ethContractAddress, provider)
+			await ethContract.approve(whitelistContractAddressInFelt, [1000000000000000, 0])
+			alert("You've approved 0.01 eth")
+		} catch (e) {
+			alert(e.message)
+		}
+	}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <main className="main">
-          <h1 className="title">
-            Starknet<a href="#"> ENS</a>
-          </h1>
-          {
-            isConnected ? 
-            <button className="connect">{address.slice(0, 5)}...{address.slice(60)}</button> :
-            <button className="connect" onClick={() => connectWallet()}>Connect wallet</button>
-          }
+	const register = async () => {
+		try {
+			const whitelistContract = new Contract(whitelistPresaleAbi, whitelistContractAddress, provider)
+			await whitelistContract.register()
+			alert("Registered!")
+		}
+		catch (e) {
+			alert(e.message)
+		}
+	}
 
-          <p className="description">
-            This demo app demonstrates the use of starknet.js to interact with starknet contracts
-          </p>
+	const claim = async () => {
+		try {
+			const whitelistContract = new Contract(whitelistPresaleAbi, whitelistContractAddress, provider)
+			await whitelistContract.claim();
+			alert("Claimed!")
+		}
+		catch (e) {
+			alert(e.message)
+		}
 
-          <div className="grid">
-            <div href="#" className="card">
-              <h2>Ensure to connect to Alpha-goerli! &rarr;</h2>
-              <p>What name do you want?.</p>
+	}
 
-              <div className="cardForm">
-                <input type="text" className="input" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} />
-
-                <input type="submit" className="button" value="Store Name" onClick={() => setNameFunction()} />
-              </div>
-
-              <hr />
-
-              <p>Insert a wallet address, to retrieve its name.</p>
-              <div className="cardForm">
-                <input type="text" className="input" placeholder="Enter Address" onChange={(e) => setInputAddress(e.target.value)} />
-
-                <input type="submit" className="button" value="Get Name" onClick={() => getNameFunction()} />
-              </div>
-              <p>Name: {retrievedName}.eth</p>
-            </div>
-          </div>
-        </main>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<main className="main">
+					<h1 className="title">
+						Week5<a>Submittion</a>
+					</h1>
+					{
+						isConnected ?
+							<button className="connect">{address.slice(0, 5)}...{address.slice(60)}</button> :
+							<button className="connect" onClick={() => connectWallet()}>Connect wallet</button>
+					}
+					<div className="grid">
+						<div href="#" className="card">
+							<p>register</p>
+							<div className="cardForm">
+								<button className='connect' onClick={() => approveEth()}>approve</button>
+							</div>
+							<div className="cardForm">
+								<button className='connect' onClick={() => register()}>register</button>
+							</div>
+							<hr />
+							<p>Claim</p>
+							<div className="cardForm">
+								<button className='connect' onClick={() => claim()}>claim</button>
+							</div>
+						</div>
+					</div>
+				</main>
+			</header>
+		</div>
+	);
 }
 
 export default App;
